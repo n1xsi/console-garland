18c0e44b1bb5e5b73175d03a397247ab8255df4d
n1xs1 <vanyalebedev45@gmail.com>
2025-10-26 23:11:42 +0300

    Small refactor

--- garland.py
+++ garland.py
@@ -1,8 +1,10 @@
 from colorama import init, Fore, Style
+from random import choice
+
 
 class Garland:
-    def __init__(self, garland_length):
-        self.colos = {
+    def __init__(self, garland_length: int = 15):
+        self.colors = {
             "red": Fore.RED,
             "green": Fore.GREEN,
             "yellow": Fore.YELLOW,
@@ -11,17 +13,18 @@ class Garland:
             "cyan": Fore.CYAN,
             "white": Fore.WHITE,
         }
-        
+
         self.bulb = "●"
         self.wire = "-"
         self.garland_length = garland_length
-        
-        self.garland = "-" + f"{self.bulb}-"*self.garland_length
+
+        # self.garland = "-" + f"{self.bulb}-"*self.garland_length
 
 
 def main():
     garland = Garland(garland_length=1)
     print(garland.garland)
+
 
 if __name__ == "__main__":
     init()
