fabac4ff69e3a9b889598010a0f4bc8ce6e6c6a2
n1xs1 <vanyalebedev45@gmail.com>
2025-10-28 21:54:44 +0300

    Added some refactor + output and coloring functions

--- garland.py
+++ garland.py
@@ -1,9 +1,16 @@
 from colorama import init, Fore, Style
 from random import choice
+from time import sleep
+import os
 
 
 class Garland:
-    def __init__(self, garland_length: int = 15):
+    """
+    Класс Гирлянды
+    
+    garland_length - длина гирлянды
+    """
+    def __init__(self, garland_length: int = 25) -> None:
         self.colors = {
             "red": Fore.RED,
             "green": Fore.GREEN,
@@ -17,15 +24,38 @@ class Garland:
         self.bulb = "●"
         self.wire = "-"
         self.garland_length = garland_length
+        self.garland = "-" + f"{self.bulb}-"*self.garland_length
 
-        # self.garland = "-" + f"{self.bulb}-"*self.garland_length
+    def print_garland(self) -> None:
+        print(f"\r{self.garland}", end="")
+        
+    def colorize_random(self) -> str:
+        garland = "-"
+        for _ in range(self.garland_length):
+            garland += f"{choice(list(self.colors.values()))}{self.bulb}{self.wire}"
+        self.garland = garland + f"{Style.RESET_ALL}"
+
+
+def clear_console():
+    """Очищает консоль в зависимости от ОС"""
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def main():
-    garland = Garland(garland_length=1)
-    print(garland.garland)
+    """Главная функция, которая выводит гирлянду в консоль"""
+    garland = Garland()
+    
+    print("🎄 Новогодняя гирлянда (Ctrl+C для выключения)")  
+    try:
+        while True:
+            garland.colorize_random()
+            garland.print_garland()
+            sleep(0.3)
+    except KeyboardInterrupt:
+        print("\nГирлянда выключена!")
 
 
 if __name__ == "__main__":
     init()
+    clear_console()
     main()
