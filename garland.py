47eb853ac1cd28a23dee89841c781eb03b61ca9f
n1xs1 <vanyalebedev45@gmail.com>
2025-11-09 21:42:48 +0300

    Small text fixes

--- garland.py
+++ garland.py
@@ -8,40 +8,56 @@ import os
 class Garland:
     """
     Класс Гирлянды с переключаемыми режимами анимации.
-    
+
     garland_length - длина гирлянды
     """
+
     def __init__(self, garland_length: int = 25) -> None:
-        self.colors = list(Fore.__dict__.values())[15:21] # [Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN]
+        """
+        Конструктор класса Гирлянды.
+        """
+        self.colors = list(Fore.__dict__.values())[15:21]
+        # [Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN]
 
         self.bulb = "●"
         self.wire = "-"
         self.garland_length = garland_length
-        
+
         self.garland = "-" + f"{self.bulb}-"*self.garland_length
 
     def print_garland(self) -> None:
+        """Выводит гирлянду в консоль"""
         print(f"\r{self.garland}", end="")
-        
-    def colorize_random(self) -> str:
+
+    def colorize_random(self):
+        """Рандомно раскрасшивает гирлянду"""
         garland = "-"
         for _ in range(self.garland_length):
-            garland += f"{choice(list(self.colors.values()))}{self.bulb}"  # Добавление цветной "лампочки"
-            garland += f"{Style.RESET_ALL}{self.wire}"                     # Добавление бесцветного "проводка"
+            # Добавление цветной "лампочки"
+            garland += f"{choice(list(self.colors.values()))}{self.bulb}"
+            # Добавление бесцветного "проводка"
+            garland += f"{Style.RESET_ALL}{self.wire}"
         self.garland = garland
+
+    def _initialize_colors(self) -> list:
+        """
+        Генерирует последовательность цветов для гирлянды так,
+        чтобы два соседних цвета не повторялись.
+        """
+        pass
 
 
 def clear_console():
-    """Очищает консоль в зависимости от ОС"""
-    os.system('cls' if os.name == 'nt' else 'clear')
+    """Очищает консоль"""
+    os.system('cls||clear')
 
 
 def main():
-    """Главная функция, которая выводит гирлянду в консоль"""
+    """Главная функция, которая имитирует работу гирлянды."""
     clear_console()
     garland = Garland()
-    
-    print("🎄 Гирлянда (ENTER - switch, Ctrl+C - exit)")  
+
+    print("🎄 Гирлянда (ENTER - switch, Ctrl+C - exit)")
     try:
         while True:
             garland.colorize_random()
@@ -54,5 +70,6 @@ def main():
 
 
 if __name__ == "__main__":
-    init(autoreset=True) # autoreset=True - чтобы не писать Style.RESET_ALL постоянно
+    """Запуск программы"""
+    init(autoreset=True)  # autoreset - чтобы не писать Style.RESET_ALL постоянно
     main()
